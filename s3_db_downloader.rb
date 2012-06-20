8505ca7ffbde5d81f7458ee42c1708b8159c4cda
eks <edu.fox@gmail.com>
2012-06-20 14:05:41 -0300

    adding the util files

---
+++ s3_db_downloader.rb
@@ -0,0 +1,35 @@
+# author: Felipe JAPM
+# created_at: 29/03/2012 - 15:00
+
+require 'aws/s3'
+require 'yaml'
+
+puts "------------ Removing previous generated backup ------------"
+system "rm -R sql_backup"
+
+config = YAML::load(File.open('aws_s3.yml'))
+
+ AWS::S3::Base.establish_connection!(
+    :access_key_id     => config["s3"]["access_key_id"],
+    :secret_access_key => config["s3"]["secret_access_key"]
+ )
+
+s3_date_format = Time.now.year.to_s + "." + '%02d' % Time.now.month + "." + '%02d' % Time.now.day
+pattern = /\/sql_backup\/#{s3_date_format}\.[0-9]{2}\.[0-9]{2}\.[0-9]{2}\/sql_backup.tar.bz2/
+
+s3_bucket_files = AWS::S3::Bucket.find('olook_sql_backups').object_cache.inspect
+file_path = s3_bucket_files.match(pattern,s3_bucket_files.rindex("#")).to_s
+puts "------------ Downloading last dump #{file_path} from Amazon S3. Be patient ! ------------"
+open('sql_backup.tar.bz2', 'w') do |file|
+  AWS::S3::S3Object.stream(file_path, 'olook_sql_backups') do |chunk|
+    file.write chunk
+  end
+end
+
+puts "------------ Descompressing file ------------"
+system 'tar -xvf sql_backup.tar.bz2'
+
+puts "------------ Restoring database #{config['mysql']['database']} ------------"
+system "mysql -hlocalhost -u#{config['mysql']['user']} -p#{config['mysql']['password']} #{config['mysql']['database']} < #{Dir.getwd}/sql_backup/MySQL/olook_production.sql"
+
+puts "Ready!"
