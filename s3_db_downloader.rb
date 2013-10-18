afc9c7c34de5a94f7be3d4f07211139f0b6067e5
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-10-18 16:54:45 -0300

    Trying to fix backup problems

--- s3_db_downloader.rb
+++ s3_db_downloader.rb
@@ -1,4 +1,4 @@
-# author: Felipe JAPM
+# author: Felipe JAPM | Tiago Almeida
 # created_at: 29/03/2012 - 15:00
 
 require 'aws/s3'
@@ -12,26 +12,22 @@ AWS::S3::Base.establish_connection!(
   :secret_access_key => config["s3"]["secret_access_key"]
 )
 
-file_name = 'sql_backup.tar.bz2'
-file_date = Time.now - 15 * 24 * 60 * 60
+file_name = 'sql_backup.tar'
+#file_date = Time.now - 15 * 24 * 60 * 60
 file_path, file_etag, file_size = ''
 
 s3_bucket_files = AWS::S3::Bucket.find('olook_sql_backups').object_cache
-s3_bucket_files.each do |file|
-  begin
-    if file.key =~ /\/#{file_name}\Z/
-      current_date = Time.parse(file.about['last-modified'].split(',')[1].gsub('GMT',''))
-      if current_date > file_date
-        file_date = current_date
-        file_path = file.key
-        file_etag = file.about['etag'].gsub('"','')
-        file_size = file.about['content-length'].to_i
-      end
-    end
-  rescue
 
+file_backup = s3_bucket_files.map{|file| file if file.key.match( /\/#{file_name}\Z/)}.compact.last
+begin
+  if file_backup
+    file_path = file_backup.key
+    file_etag = file_backup.about['etag'].gsub('"','')
+    file_size = file_backup.about['content-length'].to_i
   end
+rescue
 end
+
 
 puts "Found values:" if file_path || file_etag || file_size
 puts file_path if file_path
@@ -56,12 +52,12 @@ if File.exists?(file_name)
     system "rm -R sql_backup"
     system "rm #{file_name}"
   end
-elsif File.exists?("sql_#{file_etag}.tar.bz2")
+elsif File.exists?("sql_#{file_etag}.tar")
   # file_name = "sql_#{file_etag}.tar.bz2"
-  downloaded_digest = Digest::MD5.file("sql_#{file_etag}.tar.bz2")
+  downloaded_digest = Digest::MD5.file("sql_#{file_etag}.tar")
 end
 
-if !File.exists?(file_name) && !File.exists?("sql_#{file_etag}.tar.bz2") || downloaded_digest != file_etag
+if !File.exists?(file_name) && !File.exists?("sql_#{file_etag}.tar") || downloaded_digest != file_etag
   puts "------------ Downloading last dump #{file_path} from Amazon S3. Be patient ! ------------"
   open(file_name, 'w') do |file|
     AWS::S3::S3Object.stream(file_path, 'olook_sql_backups') do |chunk|
